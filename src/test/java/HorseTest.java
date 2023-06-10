cc8ed7fbab0a78b7ec306f69e9ffa0bab0d37d33
alicemartynova94 <alicemartynova94@gmail.ru>
2023-06-10 21:41:52 +0300

    add junit-jupiter-params dependency, throwExceptionWhenIncorrectName and checkExceptionMessageWhenIncorrectName to HorseTest

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +18,20 @@ class HorseTest {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0, 0));
         assertEquals("Name cannot be null.", exception.getMessage());
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  ", "\t"})
+    void throwExceptionWhenIncorrectName(String incorrectOptions) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(incorrectOptions, 0, 0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  ", "\t"})
+    void checkExceptionMessageWhenIncorrectName(String incorrectOptions) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse(incorrectOptions, 0, 0));
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
+
     @Test
     void getName() {
     }
