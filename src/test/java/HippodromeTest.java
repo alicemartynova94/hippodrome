8543d7d608492634794049450235e39c1f760125
alicemartynova94 <alicemartynova94@gmail.ru>
2023-06-10 18:23:01 +0300

    add junit dependency and hippodrome test class

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HippodromeTest {
+
+    @org.junit.jupiter.api.Test
+    void getHorses() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWinner() {
+    }
+}
