cbd8f56012ff22d98152a3ce93d9abc70425a52f
alicemartynova94 <alicemartynova94@gmail.ru>
2023-06-10 19:51:33 +0300

    add throwExceptionWhenNullName method to HorseTest

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,9 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
 
+    @Test
+    void throwExceptionWhenNullName() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0, 0));
+    }
+    
     @Test
     void getName() {
     }
