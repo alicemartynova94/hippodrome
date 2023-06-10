4ed8e9db08f05c202c0540be38664f4f68d4b078
alicemartynova94 <alicemartynova94@gmail.ru>
2023-06-10 20:14:32 +0300

    add checkExceptionMessageWhenNameIsNull method to HorseTest

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -10,7 +10,12 @@ class HorseTest {
     void throwExceptionWhenNullName() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0, 0));
     }
-    
+
+    @Test
+    void checkExceptionMessageWhenNameIsNull() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0, 0));
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
     @Test
     void getName() {
     }
