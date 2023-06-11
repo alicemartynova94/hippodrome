ab9a7b26cf7605da4fd17fb87f83992ab245fb76
alicemartynova94 <alicemartynova94@gmail.ru>
2023-06-11 17:58:13 +0300

    add throwExceptionWhenConstructorIsNull and checkExceptionMessageWhenConstructorIsNull to HippodromeTest

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,16 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void throwExceptionWhenConstructorIsNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+    @Test
+    void checkExceptionMessageWhenConstructorIsNull() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+        assertEquals("Horses cannot be null.", exception.getMessage());
+    }
+   
     void getHorses() {
     }
 
-    @org.junit.jupiter.api.Test
+
     void move() {
     }
 
-    @org.junit.jupiter.api.Test
+
     void getWinner() {
     }
 }
