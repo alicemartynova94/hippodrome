e0d834a0f6d421621cd34927f26b5eaee940f471
alicemartynova94 <alicemartynova94@gmail.ru>
2023-06-11 18:27:23 +0300

    add throwExceptionWhenListInConstructorIsEmpty method to HippodromeTest

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,20 +1,36 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class HippodromeTest {
 
     @Test
     void throwExceptionWhenConstructorIsNull() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
     }
+
     @Test
     void checkExceptionMessageWhenConstructorIsNull() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
         assertEquals("Horses cannot be null.", exception.getMessage());
     }
-   
+
+    @Test
+    void throwExceptionWhenListInConstructorIsEmpty() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
+    }
+
     void getHorses() {
     }
 
